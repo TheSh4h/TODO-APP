6e4cf1948e89d11f976bd46361a65dac5c4653ae
TheSh4h <hh.haseebshah@gmail.com>
2023-05-27 12:16:28 -0700

    Styled up a bit

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,17 +3,19 @@ const TodoInput = ({todo, setTodo, addTodo}) => {
         <>
             <form onSubmit={addTodo}>
 
-            <input className="m-1 bg-slate-200 rounded-lg focus:outline-none
-            border-2
+            <input className="m-5 bg-slate-200 rounded-lg focus:outline-none
+            border-2 w-80 text-black
             focus:border-teal-300 focus:border-2" 
              type="text"
              onChange={(e) => setTodo(e.target.value)}
              value={todo}/>
 
-             <input type="submit" value={"ADD"} className="font-light
-              bg-teal-800 text-white  rounded-xl
-               p-1 ml-2 cursor-pointer
-             hover:bg-teal-600 active:bg-green-950
+             <input type="submit"
+            value={"ADD"} 
+             className="font-light
+            bg-teal-800 text-white  rounded-xl
+            p-1 cursor-pointer
+            hover:bg-teal-600 active:bg-green-950
             "/>
 
             </form>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,13 @@
 const TodoList = ({ list, remove }) => {
     return ( 
-        <>
+        <div>
 
         {
             list.length > 0 ? (
                 <ul>
                     {list.map((todo, index) => (
-                        <div className="flex flex-row justify-evenly" >
+                        <div className="flex flex-row justify-between
+                    m-10 mr-20 ml-20 lg:mr-80 lg:ml-80">
                             <li key={index}>{todo}</li>
                             <button onClick={() => remove(todo)}>Delete</button>
                         </div>
@@ -19,7 +20,7 @@ const TodoList = ({ list, remove }) => {
                 )
         }
 
-        </>
+        </div>
      );
 }
  
